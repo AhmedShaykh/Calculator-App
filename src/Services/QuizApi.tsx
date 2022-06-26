163967200e359115b9d6a82c6813662d0924290b
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-06-26 23:46:36 +0500

    It's Time To Push !

--- src/Services/QuizApi.tsx
+++ src/Services/QuizApi.tsx
@@ -7,7 +7,7 @@ export const QuizApi = async (totalQuestions: number, level: string): Promise<Qu
     const res = await fetch(`https://opentdb.com/api.php?amount=${totalQuestions}&difficulty=${level}&type=multiple`);
     let { results } = await res.json();
 
-    const quiz = results.map((questionObj: QuizType) => {
+    const quiz: QuizType[] = results.map((questionObj: QuizType) => {
         return {
             question: questionObj.question,
             answer: questionObj.correct_answer,
